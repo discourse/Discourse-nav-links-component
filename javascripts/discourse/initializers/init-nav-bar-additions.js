19e5002e418cd155ef513446a6e75a818235d411
awesomerobot <kris.aubuchon@discourse.org>
2023-05-11 13:26:53 -0400

    linting

--- javascripts/discourse/initializers/init-nav-bar-additions.js
+++ javascripts/discourse/initializers/init-nav-bar-additions.js
@@ -4,23 +4,23 @@ export default {
   name: "nav-links-component",
   initialize() {
     withPluginApi("0.8", (api) => {
-      const nmlt = settings.Nav_links,
-        sec = nmlt.split("|").map(function (item) {
+      const itemsSetting = settings.Nav_links,
+        items = itemsSetting.split("|").map(function (item) {
           return item.trim();
         });
 
-      sec.forEach(function (value) {
-        const sec = value.split(";").map(function (item) {
-            return item.trim();
+      items.forEach(function (item) {
+        const splitSec = item.split(";").map(function (section) {
+            return section.trim();
           }),
-          filter = sec[0].replace(/\s+/g, "-").toLowerCase(),
-          title = sec[1],
-          location = sec[2];
+          filter = splitSec[0].replace(/\s+/g, "-").toLowerCase(),
+          title = splitSec[1],
+          location = splitSec[2];
 
         api.addNavigationBarItem({
           name: `custom_${filter}`,
           displayName: filter,
-          title: title,
+          title,
           href: location,
           forceActive: (category, args, router) => {
             return router.currentURL.includes(location);
